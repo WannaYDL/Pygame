09ebbb60a2023ff8105a62268a9c4487bb6c1bf3
WannaYDL <42736386+WannaYDL@users.noreply.github.com>
2019-01-27 23:04:46 +0800

    Add files via upload

--- piece.py
+++ piece.py
@@ -1,4 +1,5 @@
 #TetrisGame/piece.py
+
 from settings import *
 import pygame
 class Piece():
@@ -8,7 +9,7 @@ class Piece():
         self.shape = shape
         self.screen = screen
 
-
+    #画出形状为piece的方块
     def paint(self):
         shape_template = PIECES[self.shape]
 
@@ -16,8 +17,20 @@ class Piece():
             for c in range(len(shape_template[0])):
                 if shape_template[r][c] == 'O':
                     self.draw_cell(self.x+c,self.y+r)
-
+    #绘制  方格
     def draw_cell(self,x,y):
         cell_position = (x*CELL_WIDTH+GAME_AREA_LEFT+1,
                          y*CELL_WIDTH+GAME_AREA_TOP+1)
-        cell_width_height = ()
+        cell_width_height = (CELL_WIDTH-2,CELL_WIDTH-2)
+        cell_rect = pygame.Rect(cell_position,cell_width_height)
+        pygame.draw.rect(self.screen,CELL_COLOR,cell_rect)
+    def move_right(self):
+        #方格向右移动1个单元格
+        self.x += 1
+    def move_left(self):
+        #方格向左移动一格
+        self.x -= 1
+
+    def move_down(self):
+        '''方块向下移动1格'''
+        self.y += 1
--- settings.py
+++ settings.py
@@ -4,85 +4,46 @@ SCREEN_WIDTH = 1200     #窗口宽度
 SCREEN_HEIGHT = 700     #窗口高度
 CELL_WIDTH = 40         #每个方格的像素
 GAME_AREA_WIDTH = CELL_WIDTH*10         #一行10个单元格
-GAME_AREA_HEIGTH = CELL_WIDTH*15        #一共20行
+GAME_AREA_HEIGHT = CELL_WIDTH*15        #一共15行
 GAME_AREA_LEFT = (SCREEN_WIDTH-GAME_AREA_WIDTH)//2      #游戏区域左侧的剩余宽度
-GAME_AREA_TOP = SCREEN_HEIGHT-GAME_AREA_HEIGTH          #游戏区域顶部剩余高度
+GAME_AREA_TOP = SCREEN_HEIGHT-GAME_AREA_HEIGHT          #游戏区域顶部剩余高度
 EDGE_COLOR = (0,0,0)            #游戏区域边界线的颜色
 CELL_COLOR = (100,100,100)      #单元格填充色
 BG_COLOR = (200,200,200)        #窗口背景颜色
 
 #S型方块的姿态序列。首先是未翻转的姿态，接着是向右翻转90度的姿态。再翻转90度，将回到未翻转前的姿态。
-S_SHAPE_TEMPLATE = [['.OO.',
+S_SHAPE_TEMPLATE = ['.OO.',
                      'OO..',
-                     '....'],
-                    ['.O..',
-                     '.OO.',
-                     '..O.']]
+                     '....']
 
-Z_SHAPE_TEMPLATE = [['OO..',
+Z_SHAPE_TEMPLATE = ['OO..',
                      '.OO.',
-                     '....'],
-                    ['..O.',
-                     '.OO.',
-                     '.O..']]
+                     '....']
 
-I_SHAPE_TEMPLATE = [['.O..',
+I_SHAPE_TEMPLATE = ['.O..',
                      '.O..',
                      '.O..',
-                     '.O..'],
-                    ['....',
-                     'OOOO',
-                     '....',
-                     '....']]
+                     '.O..']
 
-O_SHAPE_TEMPLATE = [['OO',
-                     'OO']]
+O_SHAPE_TEMPLATE = ['OO',
+                     'OO']
 
-J_SHAPE_TEMPLATE = [['.O.',
-                     '.O.',
-                     'OO.'],
-                    ['O..',
-                     'OOO',
-                     '...'],
-                    ['OO.',
-                     'O..',
-                     'O..'],
-                    ['OOO',
-                     '..O',
-                     '...']]
+J_SHAPE_TEMPLATE = ['..O.',
+                     '..O.',
+                     '.OO.']
 
-L_SHAPE_TEMPLATE = [['O..',
-                     'O..',
-                     'OO.'],
-                    ['...',
-                     'OOO',
-                     'O..'],
-                    ['OO.',
-                     '.O.',
-                     '.O.'],
-                    ['..O',
-                     'OOO',
-                     '...']]
+L_SHAPE_TEMPLATE = ['.O..',
+                     '.O..',
+                     '.OO.']
 
-T_SHAPE_TEMPLATE = [['.O.',
-                     'OOO',
-                     '...'],
-                    ['.O.',
-                     '.OO',
-                     '.O.'],
-                    ['...',
-                     'OOO',
-                     '.O.'],
-                    ['..O',
-                     '.OO',
-                     '..O']]
+T_SHAPE_TEMPLATE = ['.O..',
+                     'OOO.',
+                     '....']
 
-PIECES = {
-    'S': S_SHAPE_TEMPLATE,
-    'Z': Z_SHAPE_TEMPLATE,
-    'J': J_SHAPE_TEMPLATE,
-    'L': L_SHAPE_TEMPLATE,
-    'I': I_SHAPE_TEMPLATE,
-    'O': O_SHAPE_TEMPLATE,
-    'T': T_SHAPE_TEMPLATE,
-}
+PIECES = {'S': S_SHAPE_TEMPLATE,
+          'Z': Z_SHAPE_TEMPLATE,
+          'J': J_SHAPE_TEMPLATE,
+          'L': L_SHAPE_TEMPLATE,
+          'I': I_SHAPE_TEMPLATE,
+          'O': O_SHAPE_TEMPLATE,
+          'T': T_SHAPE_TEMPLATE}
